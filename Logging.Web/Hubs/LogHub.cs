844160a15a573037d8b74b598233db0f323f7fb3
jjokela <jarmo.jokela@gmail.com>
2017-07-12 23:18:57 +0200

    Added SignalR

---
+++ Logging.Web/Hubs/LogHub.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using Microsoft.AspNet.SignalR;
+
+namespace Logging.Web.Hubs
+{
+    public class LogHub : Hub
+    {
+        public static void PublishMessage(LogMessage message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<LogHub>();
+            context.Clients.All.publishMessage(message);
+        }
+    }
+}

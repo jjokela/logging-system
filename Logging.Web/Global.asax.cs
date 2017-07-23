c60af38d61df687c7668cf7378fa8f76ad7f568e
jjokela <jarmo.jokela@gmail.com>
2017-07-23 15:17:51 +0200

    SignalR json to camelCase

--- Logging.Web/Global.asax.cs
+++ Logging.Web/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Serialization;
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SignalRCamelCasing;
 using System.Web.Http;
 
 namespace Logging.Web
@@ -11,6 +14,11 @@ namespace Logging.Web
             HttpConfiguration config = GlobalConfiguration.Configuration;
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            var settings = new JsonSerializerSettings();
+            settings.ContractResolver = new SignalRContractResolver();
+            var serializer = JsonSerializer.Create(settings);
+            GlobalHost.DependencyResolver.Register(typeof(JsonSerializer), () => serializer);
         }
     }
 }
---
+++ Logging.Web/SignalRContractResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNet.SignalR.Infrastructure;
+
+namespace SignalRCamelCasing
+{
+    public class SignalRContractResolver : IContractResolver
+    {
+
+        private readonly Assembly assembly;
+        private readonly IContractResolver camelCaseContractResolver;
+        private readonly IContractResolver defaultContractSerializer;
+
+        public SignalRContractResolver()
+        {
+            defaultContractSerializer = new DefaultContractResolver();
+            camelCaseContractResolver = new CamelCasePropertyNamesContractResolver();
+            assembly = typeof(Connection).Assembly;
+        }
+
+        public JsonContract ResolveContract(Type type)
+        {
+            if (type.Assembly.Equals(assembly))
+            {
+                return defaultContractSerializer.ResolveContract(type);
+
+            }
+
+            return camelCaseContractResolver.ResolveContract(type);
+        }
+
+    }
+}
